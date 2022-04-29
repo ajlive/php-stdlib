fc7913c6425f605e99fe9e3627b55f8f8092e578
AJ Livingston <ajlive@longtall.city>
2022-04-29 14:03:07 -0600

    Add Router_test

---
+++ http/Router_test.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace http\test;
+
+use http\DefaultResponseWriter;
+use http\Request;
+use http\ResponseWriter;
+use http\Router;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+final class Router_test extends \testing\T
+{
+	public function testRoute(): void
+	{
+		$router = new Router('/');
+		$router->get('login/$', \http\F::makeHandler(handleLogin(...)));
+		$r = new Request('GET', '/login/');
+		$got = servedOutput($router, $r);
+		$want = "logging in at '/login/'";
+		if ($got !== $want) {
+			$this->fatalf('wanted: %s, got: %s', enc($want), enc($got));
+		}
+	}
+
+	public function testNoRout(): void
+	{
+		$router = new Router('/');
+		$router->get('login/$', \http\F::makeHandler(handleLogin(...)));
+		$r = new Request('GET', '/login');
+		$handler = $router->findHandler($r);
+		if (null !== $handler) {
+			$this->fatalf("found handler for bad url '%s'", $r->url);
+		}
+	}
+
+	public function testAddRoutes(): void
+	{
+		$router = new Router('/app/');
+		$router->get('login/$', \http\F::makeHandler(handleLogin(...)));
+		$routerAPI = new Router('api');
+		$routerAPI->get('users/(?P<id>\d+)/$', \http\F::makeHandler(function (ResponseWriter $w, Request $r): void {
+			['id' => $id] = $r->args;
+			$w->write("got user with id {$id}");
+		}));
+
+		$router->addRoutes($routerAPI);
+
+		$r = new Request('GET', '/app/login/');
+		$got = servedOutput($router, $r);
+		$want = "logging in at '/app/login/'";
+		if ($got !== $want) {
+			$this->fatalf('wanted: %s, got: %s', enc($want), enc($got));
+		}
+
+		$r = new Request('GET', '/app/api/users/42/');
+		$got = servedOutput($router, $r);
+		$want = 'got user with id 42';
+		if ($got !== $want) {
+			$this->fatalf('wanted: %s, got: %s', enc($want), enc($got));
+		}
+	}
+}
+
+function handleLogin(ResponseWriter $w, Request $r): void
+{
+	$w->write("logging in at '{$r->url}'");
+}
+
+function enc(string $msg): string
+{
+	return json_encode($msg, JSON_UNESCAPED_SLASHES);
+}
+
+function servedOutput(Router $router, Request $r): string
+{
+	$handler = $router->findHandler($r);
+	$w = new DefaultResponseWriter([]);
+	ob_start();
+	$handler->serve($w, $r);
+	return ob_get_clean();
+}
